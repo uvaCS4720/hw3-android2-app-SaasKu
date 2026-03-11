1aac85e7dd7dd6db3232ef226ec6a55893d8acd1
Saaswath Kumar <saaskum12@gmail.com>
2026-03-11 02:28:52 -0400

    Added data class for model

---
+++ app/src/main/java/edu/nd/pmcburne/hwapp/one/model/Game.kt
@@ -0,0 +1,20 @@
+package edu.nd.pmcburne.hwapp.one.model
+
+import java.time.LocalDateTime
+//TODO: PLEASE DON"T FORGET TO IMPLEMENT API RATE LIMITS
+// 5 requests per second
+// https://ncaa-api.henrygd.me/openapi
+// THE APPLICATION WILL LOOK WEIRD IF YOU GET RATE LIMITED
+data class Game(
+    val id: String,
+    val home: String,
+    val away: String,
+    val homeScore: Int?,
+    val awayScore: Int?,
+    val homeWon: Boolean,
+    val awayWon: Boolean,
+    val gameState: String,
+    val startDate: String,
+    val currentPeriod: String,
+    val contestClock: String,
+)
