15780a7f919b76a50f2b818c9044a52cb7c14d2e
Saaswath Kumar <saaskum12@gmail.com>
2026-03-11 03:50:00 -0400

    empty test

--- app/src/androidTest/java/edu/nd/pmcburne/hwapp/one/parseTest.kt
+++ app/src/androidTest/java/edu/nd/pmcburne/hwapp/one/parseTest.kt
@@ -11,73 +11,73 @@ class parseTest {
     @Test
     fun testParseData(){
         val json = """
-        {
-  "inputMD5Sum": "3813ff4578dd5bd5348e0302bcf1bd2d",
-  "instanceId": "379fb71fe5b9445486b5421ff576282c",
-  "updated_at": "03-17-2025 14:01:28",
-  "games": [
-    {
-      "game": {
-        "gameID": "2537402",
-        "away": {
-          "score": "70",
-          "names": {
-            "char6": "UC DAV",
-            "short": "UC Davis",
-            "seo": "uc-davis",
-            "full": "University of California, Davis"
-          },
-          "winner": false,
-          "seed": "",
-          "description": "(15-14)",
-          "rank": "",
-          "conferences": [
             {
-              "conferenceName": "Big West",
-              "conferenceSeo": "big-west"
-            }
-          ]
-        },
-        "finalMessage": "FINAL",
-        "bracketRound": "",
-        "title": "Hawaii UC Davis",
-        "contestName": "",
-        "url": "/game/6351149",
-        "network": "",
-        "home": {
-          "score": "78",
-          "names": {
-            "char6": "HAWAII",
-            "short": "Hawaii",
-            "seo": "hawaii",
-            "full": "University of Hawaii, Manoa"
-          },
-          "winner": true,
-          "seed": "",
-          "description": "(15-14)",
-          "rank": "",
-          "conferences": [
+          "inputMD5Sum": "3813ff4578dd5bd5348e0302bcf1bd2d",
+          "instanceId": "379fb71fe5b9445486b5421ff576282c",
+          "updated_at": "03-17-2025 14:01:28",
+          "games": [
             {
-              "conferenceName": "Big West",
-              "conferenceSeo": "big-west"
+              "game": {
+                "gameID": "2537402",
+                "away": {
+                  "score": "70",
+                  "names": {
+                    "char6": "UC DAV",
+                    "short": "UC Davis",
+                    "seo": "uc-davis",
+                    "full": "University of California, Davis"
+                  },
+                  "winner": false,
+                  "seed": "",
+                  "description": "(15-14)",
+                  "rank": "",
+                  "conferences": [
+                    {
+                      "conferenceName": "Big West",
+                      "conferenceSeo": "big-west"
+                    }
+                  ]
+                },
+                "finalMessage": "FINAL",
+                "bracketRound": "",
+                "title": "Hawaii UC Davis",
+                "contestName": "",
+                "url": "/game/6351149",
+                "network": "",
+                "home": {
+                  "score": "78",
+                  "names": {
+                    "char6": "HAWAII",
+                    "short": "Hawaii",
+                    "seo": "hawaii",
+                    "full": "University of Hawaii, Manoa"
+                  },
+                  "winner": true,
+                  "seed": "",
+                  "description": "(15-14)",
+                  "rank": "",
+                  "conferences": [
+                    {
+                      "conferenceName": "Big West",
+                      "conferenceSeo": "big-west"
+                    }
+                  ]
+                },
+                "liveVideoEnabled": false,
+                "startTime": "12:00AM ET",
+                "startTimeEpoch": "1740891600",
+                "bracketId": "",
+                "gameState": "final",
+                "startDate": "03-02-2025",
+                "currentPeriod": "FINAL",
+                "videoState": "",
+                "bracketRegion": "",
+                "contestClock": "0:00"
+              }
             }
-          ]
-        },
-        "liveVideoEnabled": false,
-        "startTime": "12:00AM ET",
-        "startTimeEpoch": "1740891600",
-        "bracketId": "",
-        "gameState": "final",
-        "startDate": "03-02-2025",
-        "currentPeriod": "FINAL",
-        "videoState": "",
-        "bracketRegion": "",
-        "contestClock": "0:00"
-      }
-    }
-  ],
-  "hideRank": false
-}
+          ],
+          "hideRank": false
+        }
     """.trimIndent()
 
         val games = parseData(json, gender="men")
@@ -90,6 +90,21 @@ class parseTest {
         assertEquals("final", games[0].gameState)
         assertEquals("03-02-2025", games[0].startDate)
         assertEquals("men", games[0].gender)
+    }
+
+    @Test
+    fun parseEmptyTest(){
+        val json = """
+            {
+              "inputMD5Sum": "d751713988987e9331980363e24189ce",
+              "instanceId": "d41d8cd98f00b204e9800998ecf8427e",
+              "updated_at": "2026-03-11 07:12:11",
+              "games": []
+            }
+            
+    """.trimIndent()
+        val games = parseData(json, gender = "men")
+        assertEquals(0, games.size)
     }
 
 
