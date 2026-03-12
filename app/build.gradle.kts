9df9646dbd59561699f27c0e667c032758bd5fd1
Saaswath Kumar <saaskum12@gmail.com>
2026-03-12 02:04:25 -0400

    Added dao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
@@ -42,6 +43,14 @@ android {
 }
 
 dependencies {
+    val room_version = "2.8.4"
+
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    implementation(libs.androidx.room.rxjava2)
+    implementation(libs.androidx.room.paging)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/edu/nd/pmcburne/hwapp/one/model/Game.kt
+++ app/src/main/java/edu/nd/pmcburne/hwapp/one/model/Game.kt
@@ -1,12 +1,18 @@
 package edu.nd.pmcburne.hwapp.one.model
 
+import androidx.room.*
 import java.time.LocalDateTime
 //TODO: PLEASE DON"T FORGET TO IMPLEMENT API RATE LIMITS
 // 5 requests per second
 // https://ncaa-api.henrygd.me/openapi
 // THE APPLICATION WILL LOOK WEIRD IF YOU GET RATE LIMITED
+
+//TODO: Cite Room
+// https://developer.android.com/codelabs/basic-android-kotlin-compose-persisting-data-room?authuser=2#0
+
+@Entity(tableName = "game_table")
 data class Game(
-    val id: String,
+    @PrimaryKey val id: String,
     val home: String,
     val away: String,
     val homeScore: Int?,
@@ -14,6 +20,7 @@ data class Game(
     val homeWon: Boolean,
     val awayWon: Boolean,
     val gameState: String,
+    val startTime: String,
     val startDate: String,
     val currentPeriod: String,
     val contestClock: String,
